2e6558d170dbfddc3cf73f9734f38f16307a8a7c
MitsosPng <jimpan2378@gmail.com>
2023-02-02 22:48:48 +0200

    Location notification

--- app/src/main/java/com/unipi/chrisavg/smartalert/MyFirebaseMessagingService.java
+++ app/src/main/java/com/unipi/chrisavg/smartalert/MyFirebaseMessagingService.java
@@ -1,25 +1,39 @@
 package com.unipi.chrisavg.smartalert;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import kotlinx.coroutines.scheduling.CoroutineScheduler;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
+public class MyFirebaseMessagingService extends FirebaseMessagingService implements LocationListener {
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    LocationManager locationManager;
+
 
     private static final String TAG = "MyFirebaseMsgService";
 
@@ -41,6 +55,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
 
         }
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
@@ -59,6 +85,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * B) User uninstalls/reinstalls the app
      * C) User clears app data
      */
+
     @Override
     public void onNewToken(@NonNull String token) {
         Log.d(TAG, "Refreshed token: " + token);
@@ -103,5 +130,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        System.out.println(location.getLatitude()+" "+location.getLongitude());
     }
 }
