b335d8f3f22374d38969cb697feec81282aaaf14
MitsosPng <jimpan2378@gmail.com>
2023-02-08 02:30:52 +0200

    changes...

--- app/src/main/java/com/unipi/chrisavg/smartalert/MyFirebaseMessagingService.java
+++ app/src/main/java/com/unipi/chrisavg/smartalert/MyFirebaseMessagingService.java
@@ -5,19 +5,26 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.Map;
-import java.util.regex.Pattern;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -44,16 +51,17 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         }
 
-        Map<String,String> data = remoteMessage.getData();
-        String title,body,mkey;
-        title=data.get("title");
-        body=data.get("body");
-        mkey=data.get("key_1");
+        Map<String, String> data = remoteMessage.getData();
+        String title, body;
+        title = data.get("title");
+        body = data.get("body");
+
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
 
-        sendNotification(title, body);
+        String k= String.valueOf(updateLocationToDB());
+        sendNotification(title, k);
 
     }
     // [END receive_message]
@@ -87,10 +95,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String messageTitle, String messageBody) {
-        /*Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 *//* Request code *//*, intent,
-                PendingIntent.FLAG_IMMUTABLE);*/
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_IMMUTABLE);
 
 
         String channelId = "fcm_default_channel";
@@ -104,8 +112,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                                 .bigText(messageBody))
                         .setSmallIcon(R.drawable.ic_notification)
                         .setSound(defaultSoundUri)
-                        .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
-                        .setLights(Color.RED, 3000, 3000);
+                        .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                        .setLights(Color.RED, 3000, 3000)
+                        .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -121,6 +130,34 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 
+
+    public double updateLocationToDB() {
+        final Location[] locationC = {new Location("")};
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return 0;
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            locationC[0] = location;
+                        }
+                    }
+
+                });
+        return locationC[0].getLatitude();
+    }
 
 
 
